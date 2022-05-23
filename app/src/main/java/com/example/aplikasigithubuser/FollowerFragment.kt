4cbf98f2fda79595923203df815a59e7ee4ec7be
anwartanjung <anwartan55@gmail.com>
2022-05-23 19:57:27 +0700

    #finishing

---
+++ app/src/main/java/com/example/aplikasigithubuser/FollowerFragment.kt
@@ -0,0 +1,58 @@
+package com.example.aplikasigithubuser
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import com.example.aplikasigithubuser.adapter.UserRecyclerAdapter
+import com.example.aplikasigithubuser.databinding.FragmentFollowerBinding
+import com.google.android.material.snackbar.Snackbar
+
+class FollowerFragment : Fragment() {
+
+    private val detailViewModel:DetailViewModel by activityViewModels()
+    private lateinit var binding: FragmentFollowerBinding
+    private lateinit var rvAdapter: UserRecyclerAdapter
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        binding = FragmentFollowerBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val username = arguments?.getString(ARG_NAME)
+
+        username?.let { detailViewModel.fetchListFollower(it) }
+
+        detailViewModel.isLoading.observe(viewLifecycleOwner,{
+
+            showLoading(it)
+        })
+
+        detailViewModel.followers.observe(viewLifecycleOwner,{users->
+            rvAdapter = UserRecyclerAdapter(users) {
+                Snackbar.make(view,it.login, Snackbar.LENGTH_SHORT).show()
+
+            }
+            binding.rvFollower.adapter = rvAdapter
+        })
+    }
+
+    private fun showLoading(it: Boolean) {
+        binding.progressBarFollower.visibility = if (it) View.VISIBLE else View.GONE
+    }
+
+    companion object {
+
+        const val ARG_NAME = "username"
+    }
+}
